9450aa3f1e418e4fa2d8d67bf1c88d76e024e9ab
uno0uno <anderson.electronico@gmail.com>
2023-06-24 22:54:13 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <form class="p-8">
+          <div>
+            <label for="date" class="block text-gray-700 text-sm font-bold mb-2">Date:</label>
+            <input type="date" id="date" class="form-input">
+          </div>
+          <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+        </form>
+      </div>
+
+      <div class="container mx-auto px-4 py-8">
+        <div class="prose">
+          <!-- Contenido Markdown aquí -->
+          <h1>Título del artículo</h1>
+    
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis auctor odio nec elit interdum congue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras eu urna nec purus convallis euismod. Proin semper justo id turpis dignissim condimentum.</p>
+    
+          <h2>Subtítulo</h2>
+    
+          <p>Sed sit amet sapien vel ligula facilisis aliquam. Morbi sed congue massa. Fusce eget nunc vitae ligula suscipit tristique. Nulla venenatis, metus non pulvinar eleifend, mi mauris tincidunt tellus, id gravida justo magna et est.</p>
+    
+          <!-- ...continúa con el contenido Markdown... -->
+        </div>
+      </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
