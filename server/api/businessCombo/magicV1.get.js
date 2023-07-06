0a342f4cebee83af9e7b135e4676a1e372088cdf
uno0uno <anderson.electronico@gmail.com>
2023-07-06 18:00:03 +0000

    fix error

--- server/api/businessCombo/magicV1.get.js
+++ server/api/businessCombo/magicV1.get.js
@@ -1,5 +1,4 @@
 export default defineEventHandler( async (event) => {
-  const MY_KV: KVNamespace = context.cloudflare.env.MY_KV;
   const query = getQuery(event)
   const combo = await $fetch(`https://back-waro-colombia-v1-update.onrender.com/user_bussines/businesssinglecombo/${encodeURIComponent(query.idCombo)}`);
   return combo
--- server/api/businessInfo/magicV1.get.js
+++ server/api/businessInfo/magicV1.get.js
@@ -1,5 +1,4 @@
 export default defineEventHandler( async (event) => {
-  const MY_KV: KVNamespace = context.cloudflare.env.MY_KV;
   const query = getQuery(event)
   const BusinessInfo = await $fetch(`https://back-waro-colombia-v1-update.onrender.com/user_bussines/businessinfo/${encodeURIComponent(query.userBusiness)}`);
   const BusinessCombos =  await $fetch(`https://back-waro-colombia-v1-update.onrender.com/user_bussines/businesscombo/${encodeURIComponent(BusinessInfo._id.$oid)}`);
