91fe6ad0fabaac9a8108eb97307e7a6851f26f41
uno0uno <anderson.electronico@gmail.com>
2023-07-06 17:21:55 +0000

    test cloudflare binding

--- server/api/businessCombo/magicV1.get.js
+++ server/api/businessCombo/magicV1.get.js
@@ -1,7 +1,6 @@
-const runtimeConfig = useRuntimeConfig()
-
 export default defineEventHandler( async (event) => {
+  const MY_KV: KVNamespace = context.cloudflare.env.MY_KV;
   const query = getQuery(event)
-  const combo = await $fetch(`${runtimeConfig.private.apiBase}/user_bussines/businesssinglecombo/${encodeURIComponent(query.idCombo)}`);
+  const combo = await $fetch(`https://back-waro-colombia-v1-update.onrender.com/user_bussines/businesssinglecombo/${encodeURIComponent(query.idCombo)}`);
   return combo
 })
--- server/api/businessInfo/magicV1.get.js
+++ server/api/businessInfo/magicV1.get.js
@@ -1,8 +1,7 @@
-const runtimeConfig = useRuntimeConfig()
-
 export default defineEventHandler( async (event) => {
+  const MY_KV: KVNamespace = context.cloudflare.env.MY_KV;
   const query = getQuery(event)
-  const BusinessInfo = await $fetch(`${runtimeConfig.private.apiBase}/user_bussines/businessinfo/${encodeURIComponent(query.userBusiness)}`);
-  const BusinessCombos =  await $fetch(`${runtimeConfig.private.apiBase}/user_bussines/businesscombo/${encodeURIComponent(BusinessInfo._id.$oid)}`);
+  const BusinessInfo = await $fetch(`https://back-waro-colombia-v1-update.onrender.com/user_bussines/businessinfo/${encodeURIComponent(query.userBusiness)}`);
+  const BusinessCombos =  await $fetch(`https://back-waro-colombia-v1-update.onrender.com/user_bussines/businesscombo/${encodeURIComponent(BusinessInfo._id.$oid)}`);
   return {info: BusinessInfo, combos: BusinessCombos}
 })
