1f5398c28a3dfd57cbbfae2c781f1edd28f748b9
uno0uno <anderson.electronico@gmail.com>
2023-07-07 21:15:17 +0000

    remove SEO from combo pages

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,6 @@
   
   <script setup>
   
-  useServerSeoMeta({
-    title: 'My Amazing Site',
-    ogTitle: 'My Amazing Site',
-    description: 'This is my amazing site, let me tell you all about it.',
-    ogDescription: 'This is my amazing site, let me tell you all about it.',
-    ogImage: 'https://example.com/image.png',
-    twitterCard: 'summary_large_image',
-  });
-  
   </script>
   
   <style>
