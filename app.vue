e0db025dd609c96dc4b19eaa4517c2a76b4b529d
uno0uno <anderson.electronico@gmail.com>
2023-07-07 20:53:12 +0000

    refactorind components add place google maps and whatsapp open api, test og facebook and ad EVN site name

--- app.vue
+++ app.vue
@@ -8,6 +8,65 @@
 
 <script setup>
 
+const route = useRoute()
+
+const url = route.path
+const siteName = process.env.APP_NAME
+const type = 'website'
+const locale = 'es-CO'
+
+
+const head = () => {
+  return [
+    {
+      tagName: 'meta',
+      name: 'og:title',
+      content: 'El condejero del desparche',
+    },
+    {
+      tagName: 'meta',
+      name: 'og:description',
+      content: 'Una linda pagina donde las personas pueden ver lo que quieren',
+    },
+    {
+      tagName: 'meta',
+      name: 'og:image',
+      content: 'https://dummyimage.com/1200x800/e600e6/ffffff' ,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:url',
+      content: url,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:site_name',
+      content: siteName,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:type',
+      content: type,
+    },
+    {
+      tagName: 'meta',
+      name: 'og:locale',
+      content: locale,
+    },
+  ]
+}
+
+useHead(head)
+
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+
 </script>
 
 <style lang="scss" scoped>
--- pages/@[userBusiness].vue
+++ pages/@[userBusiness].vue
@@ -1,29 +1,29 @@
 <script setup>
+
 const { userBusiness } = useRoute().params;
 
 const { data: business, pending, refresh, execute, error } = await useAsyncData(
-        'count', 
-        () => {
-        try {
-          return $fetch(`/api/businessInfo/magicV1?userBusiness=${userBusiness}`)
-        } catch (error) {
-          return error;
-        }
-      }
-    )
+  'profile-info', 
+  () => {
+    try {
+      return $fetch(`/api/businessInfo/magicV1?userBusiness=${userBusiness}`)
+    } catch (error) {
+      return error;
+    }
+  }
+)
 
 </script>
 
 <template>
 
-  <Head>
-    <Title>{{ business.info.name }}</Title>
-  </Head>
     <div v-if="pending">
         LOADING...
     </div>
 
-    <div class="flex flex-col gap-6" itemscope itemtype="https://schema.org/NightClub">
+    <div v-else-if="error">Error al cargar los datos: {{ error }}</div>
+
+    <div v-else class="flex flex-col gap-6" itemscope itemtype="https://schema.org/NightClub">
         <TheBusinessHeader
         v-bind:logo_bussines="business.info.logo_bussines"
         v-bind:name="business.info.name"
@@ -37,23 +37,28 @@ const { data: business, pending, refresh, execute, error } = await useAsyncData(
       <div class="mx-auto max-h-full font-principal">
 
           <div class="border-0 border-b-[1px] border-solid border-b-border-primary flex-nowrap flex overflow-x-auto overflow-y-hidden jpg-scrollbar-thumb-none mx-0">
-          <h2 class=" text-lg text-slate-900 first:ml-0 inline-flex text-m md:text-xl font-bold capitalize hover:text-text-default relative bg-transparent outline-none mx-4 mb-[-1px] border-0 border-b-[3px] border-solid pb-2 whitespace-nowrap border-b-text-link text-text-link mr-1">
-            Promociones 
-          </h2>
+            <h2 class=" text-lg text-slate-900 first:ml-0 inline-flex text-m md:text-xl font-bold capitalize hover:text-text-default relative bg-transparent outline-none mx-4 mb-[-1px] border-0 border-b-[3px] border-solid pb-2 whitespace-nowrap border-b-text-link text-text-link mr-1">
+              Promociones 
+            </h2>
           </div>
+
         <TheBusinessCombos 
           v-bind:combos="business.combos">
         </TheBusinessCombos>
       </div>
+
       <div class="">
+
           <div class="border-0 border-b-[1px] border-solid border-b-border-primary flex-nowrap flex overflow-x-auto overflow-y-hidden jpg-scrollbar-thumb-none mx-0">
-          <h2 class=" text-lg text-slate-900 first:ml-0 inline-flex text-m md:text-xl font-bold capitalize hover:text-text-default relative bg-transparent outline-none mx-4 mb-[-1px] border-0 border-b-[3px] border-solid pb-2 whitespace-nowrap border-b-text-link text-text-link mr-1">
-            FAQs de {{ business.info.name }} 
-          </h2>
+            <h2 class=" text-lg text-slate-900 first:ml-0 inline-flex text-m md:text-xl font-bold capitalize hover:text-text-default relative bg-transparent outline-none mx-4 mb-[-1px] border-0 border-b-[3px] border-solid pb-2 whitespace-nowrap border-b-text-link text-text-link mr-1">
+              FAQs de {{ business.info.name }} 
+            </h2>
           </div>
+
       </div>
-      <TheFaqs>
-      </TheFaqs>
+
+      <ProfileTheFaqs/>
+
     </div>
 
 
