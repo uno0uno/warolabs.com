aa462c3a5f1d0045a99b156479d640135084f0f6
uno0uno <anderson.electronico@gmail.com>
2023-07-07 03:19:25 +0000

    add priceRange meta to head business

--- components/TheBusinessHeader.vue
+++ components/TheBusinessHeader.vue
@@ -69,7 +69,7 @@ function displayTextStreet(value){
       <div class="flex divide-x divide-dashed" >
         <div class="flex items-center font-normal">
             <CurrencyDollarIcon class="h-6 w-6 md:h-6 md:w-6 text-slate-900"/>
-            <p class=" px-2 text-slate-900 text-m md:text-lg" itemprop="telephone">$50.000 - $200.000 </p>
+            <p class=" px-2 text-slate-900 text-m md:text-lg" itemprop="priceRange">$50.000 - $200.000 </p>
         </div>
       </div>
         <div class="flex gap-2">
