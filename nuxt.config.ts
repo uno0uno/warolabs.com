f9934bbd6e8d1d90da36934cbdfacbf08121cd15
uno0uno <anderson.electronico@gmail.com>
2023-07-07 20:53:29 +0000

    add env changes to SITE_NAME

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
   buildModules: [
     '@nuxtjs/google-fonts'
   ],
