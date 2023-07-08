546d34fbea1ca635ba22b61d8e444fc30825a849
uno0uno <anderson.electronico@gmail.com>
2023-07-08 01:02:24 +0000

    fix price

--- components/Profile/ThePriceRange.vue
+++ components/Profile/ThePriceRange.vue
@@ -9,7 +9,7 @@ import { CurrencyDollarIcon } from '@heroicons/vue/24/solid'
         <div class="flex items-center font-normal">
             <CurrencyDollarIcon class="h-6 w-6 md:h-6 md:w-6 text-slate-900"/>
             <p itemprop="acceptedCurrency" content="COP"></p>
-            <p class=" px-2 text-slate-900 text-m md:text-lg" itemprop="priceRange">50.000 - 200.000 </p>
+            <p class=" px-2 text-slate-900 text-m md:text-lg" itemprop="priceRange">$50.000 - $200.000 </p>
         </div>
       </div>
 
