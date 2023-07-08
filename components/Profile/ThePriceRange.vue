4e3839ca6941ce9fa1b2b5d79055205dc853753c
uno0uno <anderson.electronico@gmail.com>
2023-07-08 01:08:43 +0000

    test currency accepted

--- components/Profile/ThePriceRange.vue
+++ components/Profile/ThePriceRange.vue
@@ -8,7 +8,7 @@ import { CurrencyDollarIcon } from '@heroicons/vue/24/solid'
     <div class="flex divide-x divide-dashed" >
         <div class="flex items-center font-normal">
             <CurrencyDollarIcon class="h-6 w-6 md:h-6 md:w-6 text-slate-900"/>
-            <p itemprop="acceptedCurrency" content="COP"></p>
+            <p itemprop="currenciesAccepted" content="COP"></p>
             <p class=" px-2 text-slate-900 text-m md:text-lg" itemprop="priceRange">$50.000 - $200.000 </p>
         </div>
       </div>
