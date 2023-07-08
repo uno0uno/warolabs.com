f0a9a5603e1d6d86b5a26fe3b3e706bcbb6cadf6
uno0uno <anderson.electronico@gmail.com>
2023-07-08 00:36:43 +0000

    add currenci to bussines price range

--- components/Profile/ThePriceRange.vue
+++ components/Profile/ThePriceRange.vue
@@ -9,6 +9,7 @@ import { CurrencyDollarIcon } from '@heroicons/vue/24/solid'
         <div class="flex items-center font-normal">
             <CurrencyDollarIcon class="h-6 w-6 md:h-6 md:w-6 text-slate-900"/>
             <p class=" px-2 text-slate-900 text-m md:text-lg" itemprop="priceRange">$50.000 - $200.000 </p>
+            <p itemprop="acceptedCurrency">COP</p>
         </div>
       </div>
 
