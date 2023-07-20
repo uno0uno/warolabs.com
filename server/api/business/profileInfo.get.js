e17b0665b7efb724340735bc05d6456a906f4022
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-07-20 23:08:34 +0000

    1 file modified test single text

--- server/api/business/profileInfo.get.js
+++ server/api/business/profileInfo.get.js
@@ -1,20 +1,22 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-export default defineEventHandler( async (event) => {
-  const query = getQuery(event)
-  const supabase = createClient( process.env.NUXT_SUPABASE_URL,process.env.NUXT_SUPABASE_ANON_KEY)
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event);
+  const supabase = createClient(
+    process.env.NUXT_SUPABASE_URL,
+    process.env.NUXT_SUPABASE_ANON_KEY
+  );
   const { data, error } = await supabase
-  .from('night_clubs')
-  .select('*,promos_business(id,name,description,slug),opening_hours(*)')
-  .eq('user_name', query.user_name)
-  .is('is_active', true)
-  .is('promos_business.active', true)
+    .from('night_clubs')
+    .select('*,promos_business(id,name,description,slug),opening_hours(*)')
+    .eq('user_name', query.user_name)
+    .is('is_active', true)
+    .is('promos_business.active', true);
 
-  if (error){
-    return error
+  if (error) {
+    return error;
   }
-  if(data){
-    return data
+  if (data) {
+    return data;
   }
-
-})
+});
