b280f666b516b6ca25d2c87b1c530c384a9f289c
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-07-20 22:34:39 +0000

    1 file modified test sitemaps

--- server/api/_sitemap-urls.ts
+++ server/api/_sitemap-urls.ts
@@ -1,7 +1,19 @@
 //server/api/_sitemap-urls.ts
-export default defineEventHandler(async () => {
-  const [discotecas] = await Promise.all([$fetch('/api/business/discotecas')]);
-  return [...discotecas].map((p) => {
-    return { loc: `/discotecas/${p.user_name}`, changefreq: 'weekly' };
-  });
-});
+export default cachedEventHandler(
+  async () => {
+    const [discotecas] = await Promise.all([
+      $fetch('/api/business/discotecas'),
+    ]);
+    return [...discotecas].map((p) => {
+      return {
+        loc: `/discotecas/${p.user_name}`,
+        changefreq: 'daily',
+        priority: 0.8,
+      };
+    });
+  },
+  {
+    name: 'sitemap-dynamic-url',
+    maxAge: 60 * 10, // cache URLs for 10 minutes
+  }
+);
