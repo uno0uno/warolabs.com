e2447f5107ac4d13eff8dc6e5e3440220af55585
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-07-22 23:29:38 +0000

    1 file modified, 1 added change styles promotion

--- pages/promociones/[slug].vue
+++ pages/promociones/[slug].vue
@@ -55,92 +55,93 @@ function formatPrice(price) {
     v-else
     v-for="promo in promoData"
     :key="promo.name"
-    class="flex flex-col gap-2 mx-auto max-h-full font-principal"
+    class="flex gap-6 flex-col gap-2 mx-auto max-h-full font-principal"
   >
     <CommonTheBreadcrumb></CommonTheBreadcrumb>
     <div
       class="grid grid-cols-1 md:grid-cols-12 lg:grid-cols-12 gap-4 md:gap-10"
     >
-      <div class="md:col-span-6 lg:col-span-7 flex flex-col gap-4 md:gap-4">
+      <div class="md:col-span-6 lg:col-span-7 flex flex-col gap-6 md:gap-8">
         <PromotionTheHeadPromo
           v-bind:name_product="promo.name"
           v-bind:discoteca_info="promo.discotecas"
         >
         </PromotionTheHeadPromo>
-        <div itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-          <meta
-            itemprop="itemCondition"
-            content="https://schema.org/NewCondition"
-          />
-          <meta itemprop="availability" content="https://schema.org/InStock" />
-          <div class="flex justify-between items-center">
-            <meta itemprop="priceCurrency" content="COP" />
-            <div class="flex justify-center">
-              <meta itemprop="price" :content="promo.price" />
-              <p
-                class="text-2xl md:text-3xl font-bold font-rubik text-slate-600"
+        <div class="flex flex-col gap-4" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
+          <div >
+            <meta
+              itemprop="itemCondition"
+              content="https://schema.org/NewCondition"
+            />
+            <meta itemprop="availability" content="https://schema.org/InStock" />
+            <div class="flex justify-between items-center">
+              <meta itemprop="priceCurrency" content="COP" />
+              <div class="flex justify-center">
+                <meta itemprop="price" :content="promo.price" />
+                <p
+                  class="text-2xl md:text-3xl font-bold font-rubik text-slate-600"
+                >
+                  $
+                </p>
+                <p
+                  class="text-2xl md:text-3xl font-bold font-rubik text-slate-600"
+                >
+                  {{ formatPrice(calculateTotalPrice(promo.price)) }}
+                </p>
+              </div>
+              <button
+                @click="decreaseCounter"
+                class="
+                  w-1/6
+                  text-m
+                  font-bold
+                  py-2
+                  px-4
+                  border-2 border-slate-200
+                  text-slate-900
+                  rounded-l-lg
+                "
               >
-                $
-              </p>
-              <p
-                class="text-2xl md:text-3xl font-bold font-rubik text-slate-600"
+                <span>-</span>
+              </button>
+              <div class="w-1/6 flex justify-center border-y-2 py-2 px-4">
+                {{ counterAmountProduct }}
+              </div>
+              <button
+                @click="increaseCounter"
+                class="
+                  w-1/6
+                  text-m
+                  font-bold
+                  py-2
+                  px-4
+                  border-2 border-slate-200
+                  text-slate-900
+                  rounded-r-lg
+                "
               >
-                {{ formatPrice(calculateTotalPrice(promo.price)) }}
-              </p>
+                <span>+</span>
+              </button>
             </div>
+          </div>
+          
+          <div class="flex justify-end items-center">
             <button
-              @click="decreaseCounter"
               class="
-                w-1/6
+                w-full
                 text-m
                 font-bold
                 py-2
                 px-4
-                border-2 border-slate-200
-                text-slate-900
-                rounded-l-lg
+                bg-slate-900
+                text-white
+                rounded-lg
               "
             >
-              <span>-</span>
-            </button>
-            <div class="w-1/6 flex justify-center border-y-2 py-2 px-4">
-              {{ counterAmountProduct }}
-            </div>
-            <button
-              @click="increaseCounter"
-              class="
-                w-1/6
-                text-m
-                font-bold
-                py-2
-                px-4
-                border-2 border-slate-200
-                text-slate-900
-                rounded-r-lg
-              "
-            >
-              <span>+</span>
+              <span>Reservar en 2 clicks</span>
             </button>
           </div>
         </div>
-
-        <div class="flex justify-end items-center">
-          <button
-            class="
-              w-full
-              text-m
-              font-bold
-              py-2
-              px-4
-              bg-slate-900
-              text-white
-              rounded-lg
-            "
-          >
-            <span>Reservar en 2 clicks</span>
-          </button>
-        </div>
-
         <PromotionThePromoInfo v-bind:reazon_sell_product="promo.description">
         </PromotionThePromoInfo>
       </div>
