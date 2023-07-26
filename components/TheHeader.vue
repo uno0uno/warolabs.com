f23b5c5db24eb8d13bdb7465a6390fd34aee7815
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-07-26 22:45:13 +0000

    1 file modified test menu mobile

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,14 +1,14 @@
 <script setup></script>
 
 <template>
-  <nav class="bg-white z-20 top-0 border-b-2 pb-4">
+  <nav class="bg-white z-20 top-0 border-b-2 pb-2">
     <div
       class="
-        max-w-screen-xl
-        flex flex-wrap
+        flex flex-col sm:flex-row
         items-center
         justify-between
         mx-auto
+        gap-2
         font-principal
       "
     >
@@ -21,7 +21,6 @@
           text-lg text-slate-600
           font-bold
         "
-        style="list-style: none"
       >
         <div class="cursor-pointer">
           <NuxtLink :to="{ path: `/` }">
@@ -29,7 +28,7 @@
           </NuxtLink>
         </div>
       </div>
-      <div class="flex flex-col sm:gap-2 gap-4">
+      <div class="flex flex-col sm:gap-2 gap-1">
         <div class="flex items-center justify-end gap-2">
           <div
             class="
@@ -46,54 +45,17 @@
               <span>Registrarme </span>
             </NuxtLink>
           </div>
-          <div class="md:flex md:gap-1 hidden">
+          <div class="flex gap-1">
             <a href="https://twitter.com/saifer101_?t=gmHIJIM6DvRPJmrHB49h4g&s=09" target="_blank">
-              <LogosTheTikTok class="block md:h-8 md:w-8 cursor-pointer" />
+              <LogosTheTikTok class=" h-8 w-8 cursor-pointer" />
             </a>
             <a href="https://twitter.com/saifer101_" target="_blank">
-              <LogosTheX class="block md:h-9 md:w-9 cursor-pointer" />
+              <LogosTheX class="h-9 w-9 cursor-pointer" />
             </a>
           </div>
-
-          <button
-            data-collapse-toggle="navbar-sticky"
-            type="button"
-            class="
-              inline-flex
-              items-center
-              p-2
-              w-10
-              h-10
-              justify-center
-              text-sm text-gray-500
-              rounded-lg
-              md:hidden
-              hover:bg-gray-100
-              focus:outline-none focus:ring-2 focus:ring-gray-200
-            "
-            aria-controls="navbar-sticky"
-            aria-expanded="false"
-          >
-            <span class="sr-only">Open main menu</span>
-            <svg
-              class="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
         </div>
         <div
-          class="items-center justify-between hidden w-full md:flex md:w-auto"
+          class="items-center justify-between w-full md:flex md:w-auto"
           id="navbar-sticky"
         >
           <ul
