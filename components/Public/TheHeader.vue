84111cae046b7da082db2b090192129502b201ba
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-08-12 20:35:35 +0000

    1 file modified change  header style

--- components/Public/TheHeader.vue
+++ components/Public/TheHeader.vue
@@ -5,7 +5,7 @@
     <div
       class="
         flex
-        sm:items-center
+        items-center
         justify-between
         mx-auto
         gap-2
@@ -27,18 +27,7 @@
           </NuxtLink>
         </div>
       </div>
-      <div class="flex flex-col sm:gap-2 gap-1">
-        <div class="flex items-center sm:justify-end gap-2">
-
-          <div class="flex gap-1">
-            <a href="https://www.tiktok.com/@saifer101_" target="_blank">
-              <LogosTheTikTok class=" h-8 w-8 cursor-pointer" />
-            </a>
-            <a href="https://twitter.com/saifer101_" target="_blank">
-              <LogosTheX class="h-9 w-9 cursor-pointer" />
-            </a>
-          </div>
-        </div>
+      <div class="flex items-center sm:gap-2 gap-1">
         <div
           class="items-center justify-between w-full md:flex md:w-auto"
           id="navbar-sticky"
@@ -52,16 +41,25 @@
               items-center
               gap-4
               text-lg
-              sm:text-2xl
-              lg:text-3xl
               text-slate-900
               font-extrabold
             "
           > 
-            <li>
+            <li class="text-3xl">
               <NuxtLink :to="{ path: '/blog' }"> Blog </NuxtLink>
             </li>
           </ul>
+        </div>
+                <div class="flex items-center sm:justify-end gap-2">
+
+          <div class="flex gap-1 items-center">
+            <a href="https://www.tiktok.com/@saifer101_" target="_blank">
+              <LogosTheTikTok class="h-8 w-8 cursor-pointer" />
+            </a>
+            <a href="https://twitter.com/saifer101_" target="_blank">
+              <LogosTheX class="h-10 w-10 cursor-pointer" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
