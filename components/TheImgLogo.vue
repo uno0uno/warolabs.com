4932702d504e2833ab1f7c0ab3a99d6077aecb90
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-09-02 12:11:10 -0500

    2 files modified add type webp to images

--- components/TheImgLogo.vue
+++ components/TheImgLogo.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-    <img itemprop="image"
+    <img type="image/webp" itemprop="image"
             class="w-36 h-auto lg:w-56"
             v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/QmVVPpQfuF3JoRuTd1rtgX1sBokirW9C6QwJFxMPJCGd6a`, 'alt':`waro colombia logo`}">
 </template>
