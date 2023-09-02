1b990e2b4be27cd6884fc2f5eab982a3defe2f88
uno0uno <90988855+uno0uno@users.noreply.github.com>
2023-09-02 18:52:09 -0500

    1 file modified add h2 logo cloud

--- components/Marketing/TheLogoCloud.vue
+++ components/Marketing/TheLogoCloud.vue
@@ -6,11 +6,12 @@
 <template>
 <div class="bg-white">
   <div class="">
+    <h2 class="text-start sm:text-center text-lg font-semibold leading-8 text-slate-900">Con la confianza de los equipos más innovadores del mundo</h2>
     <div class="mx-auto mt-10 grid max-w-lg items-center gap-4 grid-cols-3 justify-items-stretch">
       
-      <img itemprop="image" type="image/webp" class="w-16 h-auto sm:w-36 sm:h-auto justify-self-center" v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/QmdVBhvkCb2THNQJLns5zBYWXEoAsv9oc7TvYebGgNZeRe`, 'alt':`sonidero bogotano logo`}">
-      <img itemprop="image" type="image/webp" class="w-12 h-auto sm:w-22 sm:h-auto justify-self-center" v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/QmZF7JQGRhJft1yoqytVetoAs7QVpVRt4pjmxQHPTtcbdb`, 'alt':`dj cas logo`}">
-      <img itemprop="image" type="image/webp" class="w-16 h-auto sm:w-36 sm:h-auto justify-self-center" v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/Qmd3akhFamtXJRyLdavXdoromb89XYL3HyY4iWodxhGeVr`, 'alt':`selina logo`}">
+      <img itemprop="image" type="image/webp" class="w-20 h-auto sm:w-36 sm:h-auto justify-self-center" v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/QmdVBhvkCb2THNQJLns5zBYWXEoAsv9oc7TvYebGgNZeRe`, 'alt':`sonidero bogotano logo`}">
+      <img itemprop="image" type="image/webp" class="w-16 h-auto sm:w-22 sm:h-auto justify-self-center" v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/QmZF7JQGRhJft1yoqytVetoAs7QVpVRt4pjmxQHPTtcbdb`, 'alt':`dj cas logo`}">
+      <img itemprop="image" type="image/webp" class="w-18 h-auto sm:w-36 sm:h-auto justify-self-center" v-bind="{'src':`https://warocolombia.infura-ipfs.io/ipfs/Qmd3akhFamtXJRyLdavXdoromb89XYL3HyY4iWodxhGeVr`, 'alt':`selina logo`}">
     </div>
   </div>
 </div>
