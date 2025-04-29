aec54af78455eacb25d6956ccbac970e3cba0e89
uno0uno <anderson.electronico@gmail.com>
2025-04-29 12:13:43 -0500

    test again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,16 +26,16 @@ export default defineNuxtConfig({
         Lato: true
       }
     }],
-    '@nuxtjs/sitemap',
+    // '@nuxtjs/sitemap',
     '@nuxtjs/robots'
   ],
   site: {
     url: 'https://warolabs.com'
   },
-  sitemap: {
-    xslTips: false,
-    sources: ['/api/sitemap']
-  },
+  // sitemap: {
+  //   xslTips: false,
+  //   sources: ['/api/sitemap']
+  // },
   robots: {
     credits: false,
     groups: [
