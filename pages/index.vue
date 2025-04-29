59ea7cefa5cac36f887971ae266ef72bf76315f7
uno0uno <anderson.electronico@gmail.com>
2025-04-29 00:14:29 -0500

    hello from internet

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,6 @@
 
 <template>
   <div class="text-center flex items-center w-full h-screen justify-center">
-    <p class="text-7xl font-bold">WAROCOL.COM</p>
+    <p class="text-7xl font-bold">WAROLABS.COM</p>
   </div>
 </template>
