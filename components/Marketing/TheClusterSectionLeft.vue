0b4e2abb34ac4088257a54cf8860fa82eb4af164
anderson.arevalo <anderson.arevalo@fastmoda.com.co>
2025-05-29 11:56:11 -0500

    test ui

--- components/Marketing/TheClusterSectionLeft.vue
+++ components/Marketing/TheClusterSectionLeft.vue
@@ -16,19 +16,19 @@ defineProps({
 </script>
 
 <template>
-    <div class="w-full h-full bg-white gap-4 flex flex-col mx-auto py-10 px-8 ">
+    <div class="w-full h-full gap-4 flex flex-col mx-auto py-10 lg:px-8 global-style">
         <div class="flex flex-col gap-2 items-left justify-center text-left">
             <p class="text-lg lg:text-2xl font-light leading-relaxed text-stone-800">{{ description }}</p>
             <h2 v-if="title" class="text-5xl font-bold text-black">{{ title }}</h2>
         </div>
         <div class="grid lg:grid-cols-2 gap-6 w-full h-full place-items-center">
             <div v-for="(item, index) in items" :key="index"
-                class="flex flex-col gap-2 w-full h-fit bg-gray-100 rounded-lg p-4 px-4">
-                <div class="flex flex-col md:flex-row gap-4 items-center">
+                class="flex flex-col gap-2 w-full h-fit bg-white rounded-lg p-4 px-4">
+                <div class="flex flex-col md:flex-row gap-4 items-LEFT">
                     <div class="flex items-center justify-center w-1/5 h-full">
                         <component :is="item.icon" class="h-18 w-18" />
                     </div>
-                    <div class="flex flex-col gap-4 items-left justify-start text-center md:text-left w-4/5">
+                    <div class="flex flex-col gap-4 items-left justify-start text-left lg:w-4/5">
                         <h3 class="text-xl font-extrabold leading-none text-black">
                             {{ item.title }}
                         </h3>
