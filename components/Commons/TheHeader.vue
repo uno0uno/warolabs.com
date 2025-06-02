e89ba545c2c52da2b69e8117c7643354994758ed
uno0uno <anderson.electronico@gmail.com>
2025-06-02 17:10:47 -0500

    add black to header

--- components/Commons/TheHeader.vue
+++ components/Commons/TheHeader.vue
@@ -5,7 +5,7 @@ import TheMenu from './TheMenu.vue';
 </script>
 
 <template>
-    <div class="flex gap-4 items-center justify-start lg:justify-center p-4 bg-slate-900 text-white sm:px-12 xl:px-12">
+    <div class="flex gap-4 items-center justify-start lg:justify-center p-4 bg-black text-white sm:px-12 xl:px-12">
         <div class="flex flex-col lg:flex-row items-start lg:items-center w-full h-full justify-start lg:justify-between gap-8 lg:min-w-[48rem] max-w-6xl">
             <TheLogo />
             <TheMenu />
