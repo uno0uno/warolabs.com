b48874d395a98f3328370184e39a25b4cba9d8a6
anderson.arevalo <anderson.arevalo@fastmoda.com.co>
2025-06-18 17:58:49 -0500

    add reserved rigth

--- components/Commons/TheFooter.vue
+++ components/Commons/TheFooter.vue
@@ -6,8 +6,8 @@
 <template>
     <div class="flex flex-col gap-4 items-center justify-center p-4 bg-black text-white">
         <div class="flex flex-col gap-2 items-center justify-center">
-            <p class="text-sm font-light leading-relaxed text-stone-400 mb-2">© 2023 Your Company. All rights reserved.</p>
-            <p class="text-sm font-light leading-relaxed text-stone-400 mb-2">Privacy Policy | Terms of Service</p>
+            <p class="text-sm font-light leading-relaxed text-stone-400 mb-2">© 2025 Waro Labs. Todos los derechos reservados.</p>
+            <p class="text-sm font-light leading-relaxed text-stone-400 mb-2">Política de privacidad | Términos de servicio</p>
         </div>
         <div class="flex gap-4">
             <a href="#" class="text-white hover:text-gray-400">Facebook</a>
