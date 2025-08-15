7f82c61264b10ee57fbd999fc4e00d6b2f3f2001
uno0uno <anderson.electronico@gmail.com>
2025-08-14 19:45:58 -0500

    💅🏽 add new style to footer credits

--- components/Commons/TheFooter.vue
+++ components/Commons/TheFooter.vue
@@ -52,10 +52,10 @@ useHead({
 </script>
 
 <template>
-    <div class="flex flex-col gap-4 items-center justify-center p-4 bg-black text-white">
-        <div class="flex flex-col gap-2 items-center justify-center">
-            <p class="text-sm font-light leading-relaxed text-stone-400 mb-2">© 2025 Waro Labs. Todos los derechos reservados.</p>
-            <p class="text-sm font-light leading-relaxed text-stone-400 mb-2">Política de privacidad | Términos de servicio</p>
+    <div class="flex flex-col gap-2 items-left justify-start md:items-center md:justify-center p-4 bg-black text-white">
+        <div class="flex flex-col items-left justify-start md:items-center md:justify-center ">
+            <p class="text-sm font-light leading-relaxed text-stone-400">© 2025 Waro Labs. Todos los derechos reservados.</p>
+            <p class="text-sm font-light leading-relaxed text-stone-400">Política de privacidad | Términos de servicio</p>
         </div>
         <div class="flex gap-4">
             <a href="www.linkedin.com/in/anderson-eduardo-arévalo-ramírez-2b5768bb" class="text-white hover:text-gray-400" target="_blank" rel="noopener noreferrer">LinkedIn</a>
