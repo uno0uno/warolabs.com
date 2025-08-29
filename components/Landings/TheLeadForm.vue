ce0e19c0d70c4164f06e763aae5395c75f90f4b2
uno0uno <anderson.electronico@gmail.com>
2025-08-29 17:52:39 -0500

    💅🏽 fix changes ans styled from phone and country input

--- components/Landings/TheLeadForm.vue
+++ components/Landings/TheLeadForm.vue
@@ -5,16 +5,16 @@ import { useGlobalData } from '../../store/useGlobalData';
 import { useFormValidation } from '../../composables/useFormValidation';
 
 const globalData = useGlobalData();
-const { 
-    globalLoading, 
-    countries, 
-    selectedCountry, 
+const {
+    globalLoading,
+    countries,
+    selectedCountry,
     countrySearchQuery,
-    filteredCountries 
+    filteredCountries
 } = storeToRefs(globalData);
-const { 
-    setGlobalLoading, 
-    selectCountry 
+const {
+    setGlobalLoading,
+    selectCountry
 } = globalData;
 
 const props = defineProps({
@@ -59,7 +59,7 @@ const countryInputDisplay = computed({
         return selectedCountry.value ? selectedCountry.value.name : countrySearchQuery.value;
     },
     set(value) {
-        selectCountry(null); 
+        selectCountry(null);
         countrySearchQuery.value = value;
         validateField('phoneCountryCode');
     }
@@ -74,14 +74,14 @@ const validateAll = () => {
 
 const submitLead = async () => {
     serverError.value = null;
-    errors.value = {}; 
+    errors.value = {};
 
     validateAll();
-    
+
     if (!isFormValid.value) {
         return;
     }
-    
+
     globalLoading.value = true;
 
     try {
@@ -117,9 +117,9 @@ const submitLead = async () => {
         selectCountry(countries.value[0]);
 
         await navigateTo(`/thankyou/${slug}`, { replace: true });
-        
+
     } catch (error) {
-  
+
         if (error.response && error.response._data) {
             serverError.value = error.response._data.message || 'Hubo un error en el servidor. Por favor, inténtalo de nuevo.';
         } else {
@@ -143,9 +143,9 @@ const hideDropdownOnFocusOut = (event) => {
 };
 
 onMounted(() => {
-  if (selectedCountry.value) {
-    formData.value.phoneCountryCode = selectedCountry.value.phone_code;
-  }
+    if (selectedCountry.value) {
+        formData.value.phoneCountryCode = selectedCountry.value.phone_code;
+    }
 });
 
 watch(selectedCountry, (newCountry) => {
@@ -161,19 +161,18 @@ watch(selectedCountry, (newCountry) => {
 <template>
     <form @submit.prevent="submitLead" class="bg-gray-100 p-6 border-2 border-slate-900 text-left flex flex-col gap-4">
         <h2 class="text-2xl font-semibold text-left">Regístrate y recibe más información</h2>
-        
-        <div v-if="allErrorsList.length > 0" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+
+        <div v-if="allErrorsList.length > 0"
+            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
             <ul class="list-disc list-inside">
                 <li v-for="error in allErrorsList" :key="error">{{ error }}</li>
             </ul>
         </div>
-        
+
         <div class="flex flex-col gap-2">
             <div class="text-left">
                 <label for="name" class="block text-gray-700 text-sm font-bold mb-2 text-left">Nombre</label>
-                <input id="name" v-model="formData.name" type="text"
-                    :class="{'border-red-500': errors.name}"
-                    class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left
+                <input id="name" v-model="formData.name" type="text" :class="{ 'border-red-500': errors.name }" class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left
                            bg-white autofill:bg-white
                            focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent"
                     @input="validateField('name')" />
@@ -182,29 +181,27 @@ watch(selectedCountry, (newCountry) => {
             <div class="text-left">
                 <label for="email" class="block text-gray-700 text-sm font-bold mb-2 text-left">Correo
                     electrónico</label>
-                <input id="email" v-model="formData.email" type="email"
-                    :class="{'border-red-500': errors.email}"
-                    class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left
+                <input id="email" v-model="formData.email" type="email" :class="{ 'border-red-500': errors.email }" class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left
                            bg-white autofill:bg-white
                            focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent"
                     @input="validateField('email'); serverError = null" />
             </div>
 
             <div class="flex gap-2 w-full">
-                <div class="text-left w-1/3 flex flex-col">
+                <div class="text-left flex flex-col w-full">
                     <label for="phone" class=" text-gray-700 text-sm font-bold mb-2 text-left">País</label>
-                    <div class="flex flex-row gap-2 relative w-full" ref="countryDropdownRef" @focusin="showCountryDropdown = true" @focusout="hideDropdownOnFocusOut">
+                    <div class="flex flex-row gap-2 relative w-full" ref="countryDropdownRef"
+                        @focusin="showCountryDropdown = true" @focusout="hideDropdownOnFocusOut">
                         <div class="relative w-full">
-                            <input v-model="countryInputDisplay"
-                                    :class="{'border-red-500': errors.phoneCountryCode}"
-                                    class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left bg-white focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent"
-                                    placeholder="País" />
+                            <input v-model="countryInputDisplay" :class="{ 'border-red-500': errors.phoneCountryCode }"
+                                class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left bg-white focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent"
+                                placeholder="País" />
 
-                            <div v-if="showCountryDropdown || countrySearchQuery.length > 0" class="absolute z-10 w-full bg-white border border-gray-300 mt-1 rounded shadow-lg max-h-48 overflow-y-auto">
+                            <div v-if="showCountryDropdown || countrySearchQuery.length > 0"
+                                class="absolute z-10 w-full bg-white border border-gray-300 mt-1 rounded shadow-lg max-h-48 overflow-y-auto">
                                 <ul>
-                                    <p v-for="country in filteredCountries" 
-                                        :key="country.code" 
-                                        @mousedown.prevent="handleCountrySelection(country)" 
+                                    <p v-for="country in filteredCountries" :key="country.code"
+                                        @mousedown.prevent="handleCountrySelection(country)"
                                         class="cursor-pointer px-3 pt-0.5 hover:bg-gray-100 text-sm">
                                         {{ country.name }}
                                     </p>
@@ -214,9 +211,9 @@ watch(selectedCountry, (newCountry) => {
                     </div>
                 </div>
                 <div class="text-left w-full flex flex-col">
-                    <label for="phone" class="block text-gray-700 text-sm font-bold mb-2 text-left">Número de teléfono</label>
-                    <input id="phone" v-model="formData.phone" type="tel"
-                        :class="{'border-red-500': errors.phone}"
+                    <label for="phone" class="block text-gray-700 text-sm font-bold mb-2 text-left">Número de
+                        teléfono</label>
+                    <input id="phone" v-model="formData.phone" type="tel" :class="{ 'border-red-500': errors.phone }"
                         class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight text-left
                                bg-white autofill:bg-white
                                focus:outline-none focus:ring-2 focus:ring-gray-700 focus:border-transparent"
@@ -225,9 +222,8 @@ watch(selectedCountry, (newCountry) => {
             </div>
         </div>
 
-        <button type="submit"
-            :disabled="!isFormValid"
-            :class="{'bg-slate-300 cursor-not-allowed': !isFormValid, 'bg-slate-800 hover:bg-slate-900': isFormValid}"
+        <button type="submit" :disabled="!isFormValid"
+            :class="{ 'bg-slate-300 cursor-not-allowed': !isFormValid, 'bg-slate-800 hover:bg-slate-900': isFormValid }"
             class="text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full text-left">
             Enviar
         </button>
